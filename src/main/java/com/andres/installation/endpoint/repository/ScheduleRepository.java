61569e2d123652cb56f904731bea84822706af50
andres <lp.carlosorjuela@gmail.com>
2025-10-09 21:58:41 -0400

    logic for the real problem

--- src/main/java/com/andres/installation/endpoint/repository/ScheduleRepository.java
+++ src/main/java/com/andres/installation/endpoint/repository/ScheduleRepository.java
@@ -9,7 +9,9 @@ import org.springframework.data.jpa.repository.Query;
 import com.andres.installation.endpoint.enitities.Schedules;
 
 public interface ScheduleRepository extends JpaRepository<Schedules,Long> {
-    
-   
+
+    //the query should be here
+    @Query("SELECT s FROM Schedules s WHERE s.technician.id = ?1 AND ((?2 BETWEEN s.startDate AND s.endDate) OR (?3 BETWEEN s.startDate AND s.endDate) OR (s.startDate BETWEEN ?2 AND ?3) OR (s.endDate BETWEEN ?2 AND ?3))")
+    List<Schedules> findConflictingSchedules(Long technicianId, Instant newStartDate, Instant newEndDate);
 
 }
